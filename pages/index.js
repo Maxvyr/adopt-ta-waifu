86cb90b71d71e60bae40d41883462085880585f9
maxvyr <60290207+Maxvyr@users.noreply.github.com>
2024-03-11 20:37:41 +0100

    💄 update design

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,70 @@
-import { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import zeroTwoSky from '../public/zero_two_sky.jpeg'
+import { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import zeroTwoSky from "../public/zero_two_sky.jpeg";
+import IconButton from "./components/twitter-button";
 
 const Home = () => {
+  const classButton =
+    "border rounded-md border-sky-500 hover:border-sky-700 hover:rounded-lg hover:border-2 p-6";
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Adopt Ta Waifu</title>
-        <meta name="Adopt Ta Waifu" content="Adopt Ta Waifu, searhc and find your best waifu or husbando" />
+        <meta
+          name="Adopt Ta Waifu"
+          content="Adopt Ta Waifu, searhc and find your best waifu or husbando"
+        />
         <link rel="icon" href="/chibi_waifu.jpeg" />
       </Head>
 
       <main className={styles.main}>
         <Image
+          className="rounded-b-[40px] object-cover"
           src={zeroTwoSky}
           alt="zero two night view"
         />
 
-        <p className={styles.description}>
+        <p className="text-center -mt-16 pb-16">
           <Image
-          className={styles.logo}
-          src='/chibi_waifu2.png'
-          alt="chibi waifu"
-          height={120}
-          width={120}
+            className="rounded-full"
+            src="/chibi_waifu2.png"
+            alt="chibi waifu"
+            height={120}
+            width={120}
           />
         </p>
 
-        <div className={styles.grid}>
+        <div className="flex justify-center space-x-32 pb-16">
           <Link href="/waifu-sfw">
-            <a className={styles.card}>
-              <h2 className='font-bold'>SFW</h2>
+            <a className={classButton}>
+              <h2 className="font-bold">SFW</h2>
             </a>
           </Link>
-          
+
           <Link href="/waifu-nsfw">
-            <a className={styles.card}>
-              <h2 className='font-bold'>NSFW</h2>
+            <a className={classButton}>
+              <h2 className="font-bold">NSFW</h2>
             </a>
           </Link>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="flex justify-between items-center px-4 mt-5 pb-2 border-t border-gray-300">
         <a
           href="https://github.com/Maxvyr/adopt-ta-waifu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div className='underline text-gray-400'>
-            Powered by Maxvyr @2022
-          </div>
+          <div className="underline text-gray-400">Powered by Maxvyr @2022</div>
         </a>
+        <IconButton url="https://twitter.com/maxvidalinc" />
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
